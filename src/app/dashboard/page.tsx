a7f12140822be41849482edebe3940d87058cc4f
momen-samirr <momensamir759@gmail.com>
2025-02-15 17:35:48 +0200

    Add Link To navbar 12

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -46,7 +46,7 @@ const DashboardRoute = async () => {
             title="Hunkel"
             href="/dashboard/hunkel"
             veicle="ملاكي"
-            availability="Now Available"
+            availability="Available Soon"
             topic="شركات"
             subTitle="Reserved Trips"
             subHref="/dashboard/reservedTrips"
