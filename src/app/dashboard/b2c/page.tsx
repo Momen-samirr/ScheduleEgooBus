0914b819a746722723c17c5d7cba2d95ba146691
momen-samirr <momensamir759@gmail.com>
2025-02-21 12:01:25 +0200

    Update Text in b2c components

--- src/app/dashboard/b2c/page.tsx
+++ src/app/dashboard/b2c/page.tsx
@@ -25,7 +25,7 @@ const UberRoute = async () => {
             تنويه هام: أي جدول تطلبه سيتم تنزيله تلقائيًا، ولا يمكن إلغاؤه إلا
             بعد إبلاغ مستر هاني كارم.
           </p>
-          <p className="text-xl text-sky-500">جداول شغل من يوم 16 الي يوم 20</p>
+          <p className="text-xl text-sky-500">جداول شغل من يوم 23 الي يوم 27</p>
           {trips.map((trip) => (
             <PostCard
               key={trip.id}
