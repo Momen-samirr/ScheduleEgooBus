feeb7262643bdfdb2cef406ac2e3d51d8f0beb21
momen-samirr <momensamir759@gmail.com>
2025-02-27 09:36:09 +0200

    Offline b2c ramdan

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -18,7 +18,7 @@ const DashboardRoute = async () => {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-10 gap-10">
-        <div className="lg:col-span-5">
+        {/* <div className="lg:col-span-5">
           <CompanyCard
             title="جداول"
             href="/dashboard/b2cramadan"
@@ -29,7 +29,7 @@ const DashboardRoute = async () => {
             topic="توزيعة رمضان"
             src="/ramdan.png"
           />
-        </div>
+        </div> */}
         <div className="lg:col-span-5">
           <CompanyCard
             title="جداول"
