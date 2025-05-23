ac06f03d9c1dca80ef9bae6657f3f94962e361f7
momen-samirr <momensamir759@gmail.com>
2025-05-23 18:34:48 +0300

    Edit the trips date to match the days

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -42,7 +42,7 @@ const DashboardRoute = async () => {
             subHref="/dashboard/solo"
             veicle="هاي اس"
             availability="Now Available"
-            topic="جداول شغل من فتره 19 الي 24"
+            topic="جداول شغل من فتره 25 الي 31"
             src="/uber.png"
           />
         </div>
