97a5a93606e984cd6b1cca816f3a9a67c6087895
momen-samirr <momensamir759@gmail.com>
2025-06-05 14:37:01 +0300

    Edit b2c trips for new week

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -42,7 +42,7 @@ const DashboardRoute = async () => {
             subHref="/dashboard/solo"
             veicle="هاي اس"
             availability="Now Available"
-            topic="جداول شغل من فتره 1 الي 6"
+            topic="جداول شغل من فتره 8 الي 12"
             src="/uber.png"
           />
         </div>
