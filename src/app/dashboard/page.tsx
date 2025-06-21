dbf178181edd6ff64851fa5d876365fe2ab4b262
momen-samirr <momensamir759@gmail.com>
2025-06-21 13:16:36 +0300

    Close Route For now routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -66,7 +66,7 @@ const DashboardRoute = async () => {
                 title="New Henkel schedule"
                 href="/dashboard/testCard"
                 veicle="ملاكي"
-                availability="Now Available"
+                availability="Available Soon"
                 topic="شركات"
                 subTitle="My Reserved Schedule"
                 subHref="/dashboard/myRoute"
