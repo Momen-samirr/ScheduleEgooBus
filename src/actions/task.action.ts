b303f3937c4e89b38d903e682cae9fed4d421f76
momen-samirr <momensamir759@gmail.com>
2025-06-27 13:13:56 +0300

    Modify Create task logic

--- src/actions/task.action.ts
+++ src/actions/task.action.ts
@@ -11,16 +11,16 @@ export async function createTask(content: string, image: string) {
 
     if (!content || !image) return;
 
-    const currentUserWithOutReservedTask = await prisma.user.findUnique({
-      where: {
-        id: dbUser?.id,
-        comments: {
-          none: {},
-        },
-      },
-    });
+    // const currentUserWithOutReservedTask = await prisma.user.findUnique({
+    //   where: {
+    //     id: dbUser?.id,
+    //     comments: {
+    //       none: {},
+    //     },
+    //   },
+    // });
 
-    if (!currentUserWithOutReservedTask) return;
+    // if (!currentUserWithOutReservedTask) return;
     await prisma.$transaction(async (tx) => {
       await tx.task.create({
         data: {
