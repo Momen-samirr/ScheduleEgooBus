ed079b93b8af599a5a1a20b9168b2589e5100403
momen-samirr <momensamir759@gmail.com>
2025-07-31 10:03:43 +0300

    Modify the date here R

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -42,7 +42,7 @@ const DashboardRoute = async () => {
             subHref="/dashboard/solo"
             veicle="هاي اس"
             availability="Now Available"
-            topic="جداول شغل من فتره27 الي 31"
+            topic="جداول شغل من فتره3 الي 7"
             src="/uber.png"
           />
         </div>
