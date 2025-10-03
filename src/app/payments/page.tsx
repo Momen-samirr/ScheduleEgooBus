464d40698a032975ddb9a34c29c91b5c0e2711ca
momen-samirr <momensamir759@gmail.com>
2025-10-03 10:51:14 +0300

    Push new week payments for Drivers

--- src/app/payments/page.tsx
+++ src/app/payments/page.tsx
@@ -12,64 +12,72 @@ import {
 import { CalendarDays, DollarSign, Banknote } from "lucide-react";
 
 const weeks = [
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbyy2Olho--5SEZ3b0uWz4xnhyCUCLCrKjjbTwShzzmrduMtg6LKzq-HXAQtJMgWNuf_mg/exec",
+  //   label: "تفاصيل حساب أسبوع 3 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbxEg9-AdDcM8D17vGkSvMDD6QxtmUOiGp88zv6i384-kTEiFIWPgDWxfUW0zQDYkFnK_g/exec",
+  //   label: "تفاصيل حساب أسبوع 10 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbz9YuHBFzIhqagU-3Vd9Nyd2tJ0kat7SWPVhUEqDitIb_2LsWFvtskwTPHrc4lBR_bL/exec",
+  //   label: "تفاصيل حساب اسبوع 17 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbycfTVjw5kv23wABEFz0yZqAavGUJVZht9M--PJM9yNrKWE1KZocNmq39itgTRyLp6s/exec",
+  //   label: "تفاصيل حساب اسبوع 24 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbxbfgpBVFGEdIuBdh0WjCLuHlESYe0Vh25u8RU4r8VEfBuC4h423aqIWv4S1BawcdL9/exec",
+  //   label: "تفاصيل حساب اسبوع 31 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbxNV0tCOCFw_0Gt91lSYa0v1dhm1ZAs5BugHup_mVqv24wdL7wK5RM3tqt9acyvHMgWcA/exec",
+  //   label: "تفاصيل حساب اسبوع 7 - 9",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbxequAZIhWWspfK4ftvSNpIfkU4i89Aey5EGp_473f4lbI7AjRs75IC-XZfFw1hb6L79g/exec",
+  //   label: "تفاصيل حساب اسبوع 14 - 9",
+  // },
   {
-    id: "https://script.google.com/macros/s/AKfycbyy2Olho--5SEZ3b0uWz4xnhyCUCLCrKjjbTwShzzmrduMtg6LKzq-HXAQtJMgWNuf_mg/exec",
-    label: "تفاصيل حساب أسبوع 3 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbxEg9-AdDcM8D17vGkSvMDD6QxtmUOiGp88zv6i384-kTEiFIWPgDWxfUW0zQDYkFnK_g/exec",
-    label: "تفاصيل حساب أسبوع 10 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbz9YuHBFzIhqagU-3Vd9Nyd2tJ0kat7SWPVhUEqDitIb_2LsWFvtskwTPHrc4lBR_bL/exec",
-    label: "تفاصيل حساب اسبوع 17 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbycfTVjw5kv23wABEFz0yZqAavGUJVZht9M--PJM9yNrKWE1KZocNmq39itgTRyLp6s/exec",
-    label: "تفاصيل حساب اسبوع 24 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbxbfgpBVFGEdIuBdh0WjCLuHlESYe0Vh25u8RU4r8VEfBuC4h423aqIWv4S1BawcdL9/exec",
-    label: "تفاصيل حساب اسبوع 31 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbxNV0tCOCFw_0Gt91lSYa0v1dhm1ZAs5BugHup_mVqv24wdL7wK5RM3tqt9acyvHMgWcA/exec",
-    label: "تفاصيل حساب اسبوع 7 - 9",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbxequAZIhWWspfK4ftvSNpIfkU4i89Aey5EGp_473f4lbI7AjRs75IC-XZfFw1hb6L79g/exec",
-    label: "تفاصيل حساب اسبوع 14 - 9",
+    id: "https://script.google.com/macros/s/AKfycbz6T0NtjqCoSd9P0vU9hrPU1w2AniUkx80oNhq8Ab4kaamsEzeTL4IWQZoVIUX5meO0/exec",
+    label: "تفاصيل حساب اسبوع 21 - 9",
   },
 ];
 
 const btbWeeks = [
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbylkkgraB-nIqIx9JA5Z48PsF9Z1qk5qD4ztivFDsgOEXjhOg7RuX9MW05KMZknTdSI/exec",
+  //   title: "تفاصيل حساب اسبوع 3 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbwMc5vR_tUJgrTiDT934CTvTbiQuQUfa7enwnPevdXp_Llt8pvcbR2iaBVeMxRC_zo/exec",
+  //   title: "تفاصيل حساب اسبوع 10 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbxuhiclsLRU4J5105GWcCmR3Z3nzxgSGtz3KpHavh54NscWrQgFbS2xkP3wtz_nsyjq/exec",
+  //   title: "تفاصيل حساب اسبوع 17 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbwtl8kRVR8ZUoz_AmafbR1Pxe_hHmeTmhiueoj9xuWeheVIPTDcZNwgkvl2pYx9DydReQ/exec",
+  //   title: "تفاصيل حساب اسبوع 24 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbxCrcx9NJZ-mvcjsc7ESWdJHyBhybt5LyTTxSxLCU1Si7NUcruqt9dkzkD-ySFldVSe/exec",
+  //   title: "تفاصيل حساب اسبوع 31 - 8",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycby3jbc81RdakJbm4fVjj9BMHH081Qy4kP2dBRZp8g16u-oJlYQm4cv9C-imRS_vHc2E/exec",
+  //   title: "تفاصيل حساب اسبوع 7 - 9",
+  // },
+  // {
+  //   id: "https://script.google.com/macros/s/AKfycbzgQbvv48EJkjAECB3TfuxQH5xBsD_gL7v7zj1JElYYffQ0CjThWnnQ12WMkouZYmf_/exec",
+  //   title: "تفاصيل حساب اسبوع 14 - 9",
+  // },
   {
-    id: "https://script.google.com/macros/s/AKfycbylkkgraB-nIqIx9JA5Z48PsF9Z1qk5qD4ztivFDsgOEXjhOg7RuX9MW05KMZknTdSI/exec",
-    title: "تفاصيل حساب اسبوع 3 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbwMc5vR_tUJgrTiDT934CTvTbiQuQUfa7enwnPevdXp_Llt8pvcbR2iaBVeMxRC_zo/exec",
-    title: "تفاصيل حساب اسبوع 10 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbxuhiclsLRU4J5105GWcCmR3Z3nzxgSGtz3KpHavh54NscWrQgFbS2xkP3wtz_nsyjq/exec",
-    title: "تفاصيل حساب اسبوع 17 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbwtl8kRVR8ZUoz_AmafbR1Pxe_hHmeTmhiueoj9xuWeheVIPTDcZNwgkvl2pYx9DydReQ/exec",
-    title: "تفاصيل حساب اسبوع 24 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbxCrcx9NJZ-mvcjsc7ESWdJHyBhybt5LyTTxSxLCU1Si7NUcruqt9dkzkD-ySFldVSe/exec",
-    title: "تفاصيل حساب اسبوع 31 - 8",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycby3jbc81RdakJbm4fVjj9BMHH081Qy4kP2dBRZp8g16u-oJlYQm4cv9C-imRS_vHc2E/exec",
-    title: "تفاصيل حساب اسبوع 7 - 9",
-  },
-  {
-    id: "https://script.google.com/macros/s/AKfycbzgQbvv48EJkjAECB3TfuxQH5xBsD_gL7v7zj1JElYYffQ0CjThWnnQ12WMkouZYmf_/exec",
-    title: "تفاصيل حساب اسبوع 14 - 9",
+    id: "https://script.google.com/macros/s/AKfycbxzBTWQ-YHLA3Ckq4eRlM3YiEmU6FhpDEk5rJcVdYnbDANMgFWA9yZit1I0G3u2zrKEiw/exec",
+    title: "تفاصيل حساب اسبوع 21 - 9",
   },
 ];
 
