7713f7840c928866e04e937e154d8eadbcd530bb
momen-samirr <momensamir759@gmail.com>
2025-10-10 15:20:42 +0300

    Update the text that sohwed on the dasboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -30,7 +30,7 @@ const DashboardRoute = async () => {
             subHref="/dashboard/solo"
             veicle="هاي اس"
             availability="Now Available"
-            topic="جداول شغل من فتره5 الي 8"
+            topic="جداول شغل من فتره12 الي 16"
             src="/uber.png"
           />
         </div>
