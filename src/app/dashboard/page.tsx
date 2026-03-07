f6db43b9e3b5d47a0fb6c7effa5d9a761cbfb5f4
Momen-samirr <momensamir759@gmail.com>
2026-03-07 20:14:47 +0200

    Push trips here with these pro

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,18 +22,18 @@ const DashboardRoute = async () => {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-10 gap-10">
-        {/* <div className="lg:col-span-5">
+        <div className="lg:col-span-5">
           <CompanyCard
-            title="جداول رمضان"
+            title="جداول بعد رمضان"
             href="/dashboard/b2c"
-            subTitle="رحلات فردية"
-            subHref="/dashboard/solo"
+            // subTitle="رحلات فردية"
+            // subHref="/dashboard/solo"
             veicle="هاي اس"
             availability="Now Available"
-            topic="جداول شهر رمضان"
+            topic="جداول بعد رمضان"
             src="/ramdan.png"
           />
-        </div> */}
+        </div>
         <div className="lg:col-span-5">
           <CompanyCard
             title="جداول شغل"
