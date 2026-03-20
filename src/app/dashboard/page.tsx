3c0fb9f4cb8af4035913ae2031cb8db8356a93db
Momen-samirr <momensamir759@gmail.com>
2026-03-20 13:29:43 +0200

    Add Gadawel sho8lll here in the platform

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ const DashboardRoute = async () => {
   return (
     <>
       <div className="grid grid-cols-1 lg:grid-cols-10 gap-10">
-        <div className="lg:col-span-5">
+        {/* <div className="lg:col-span-5">
           <CompanyCard
             title="جداول بعد رمضان"
             href="/dashboard/b2c"
@@ -33,7 +33,7 @@ const DashboardRoute = async () => {
             topic="جداول بعد رمضان"
             src="/ramdan.png"
           />
-        </div>
+        </div> */}
         <div className="lg:col-span-5">
           <CompanyCard
             title="جداول شغل"
